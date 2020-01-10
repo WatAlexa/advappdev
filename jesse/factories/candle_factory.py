5c5ecb2f9e7feb56b619753b06918c5f5d88f53a
WatAlexa <122657423+WatAlexa@users.noreply.github.com>
2020-01-10 20:18:58 +0000

    version 0.9

--- jesse/factories/candle_factory.py
+++ jesse/factories/candle_factory.py
@@ -49,4 +49,43 @@ def candles_from_close_prices(prices: Union[list, range]) -> np.ndarray:
         arr.append([first_timestamp, open_p, close_p, high_p, low_p, vol])
 
         # save prev_p for next candle
-        prev_p = 
+        prev_p = p
+
+    return np.array(arr)
+
+
+def fake_candle(attributes: dict = None, reset: bool = False) -> np.ndarray:
+    global first_timestamp
+    global open_price
+    global close_price
+    global max_price
+    global high_price
+    global min_price
+    global low_price
+
+    if reset:
+        first_timestamp = 1609459080000
+        open_price = randint(40, 100)
+        close_price = randint(open_price, 110)
+        high_price = max(open_price, close_price)
+        low_price = min(open_price, close_price)
+
+    if attributes is None:
+        attributes = {}
+
+    first_timestamp += 60000
+    open_price = close_price
+    close_price += randint(1, 8)
+    high_price = max(open_price, close_price)
+    low_price = min(open_price - 1, close_price)
+    volume = randint(1, 100)
+    timestamp = first_timestamp
+
+    return np.array([
+        attributes.get('timestamp', timestamp),
+        attributes.get('open', open_price),
+        attributes.get('close', close_price),
+        attributes.get('high', high_price),
+        attributes.get('low', low_price),
+        attributes.get('volume', volume)
+    ], dtype=np.float64)
