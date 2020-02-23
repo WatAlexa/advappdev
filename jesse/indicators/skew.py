19bb4fb71e41782223a26616152a894e96f302c2
WatAlexa <122657423+WatAlexa@users.noreply.github.com>
2020-02-23 16:00:04 +0000

    Updated sidebar.html

--- jesse/indicators/skew.py
+++ jesse/indicators/skew.py
@@ -12,4 +12,18 @@ def skew(candles: np.ndarray, period: int = 5, source_type: str = "hl2", sequent
     """
     Skewness
 
-    
+    :param candles: np.ndarray
+    :param period: int - default: 5
+    :param source_type: str - default: "hl2"
+    :param sequential: bool - default: False
+
+    :return: float | np.ndarray
+    """
+    candles = slice_candles(candles, sequential)
+
+    source = get_candle_source(candles, source_type=source_type)
+    swv = sliding_window_view(source, window_shape=period)
+    skewness = stats.skew(swv, axis=-1)
+    res = same_length(source, skewness)
+
+    return res if sequential else res[-1]
