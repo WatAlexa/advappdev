c8a7b89d53bd62a5f129ad8496c7f5437501332e
WatAlexa <122657423+WatAlexa@users.noreply.github.com>
2020-03-21 13:01:57 +0000

    resolving

--- jesse/modes/backtest_mode.py
+++ jesse/modes/backtest_mode.py
@@ -153,4 +153,123 @@ def _get_study_name() -> str:
         strategy_name = router.routes[0].strategy_name
     else:
         strategy_name = router.routes[0].strategy_name.__name__
-    study_name = f"{strategy_name}-{router.routes[0].exchange}-{router.routes[0].symbol}-{router.routes[0].time
+    study_name = f"{strategy_name}-{router.routes[0].exchange}-{router.routes[0].symbol}-{router.routes[0].timeframe}{more}"
+    return study_name
+
+
+def load_candles(start_date_str: str, finish_date_str: str) -> Dict[str, Dict[str, Union[str, np.ndarray]]]:
+    start_date = jh.date_to_timestamp(start_date_str)
+    finish_date = jh.date_to_timestamp(finish_date_str) - 60000
+
+    # validate
+    if start_date == finish_date:
+        raise ValueError('start_date and finish_date cannot be the same.')
+    if start_date > finish_date:
+        raise ValueError('start_date cannot be bigger than finish_date.')
+    if finish_date > arrow.utcnow().int_timestamp * 1000:
+        raise ValueError(
+            "Can't load candle data from the future! The finish-date can be up to yesterday's date at most.")
+
+    # load and add required warm-up candles for backtest
+    if jh.is_backtesting():
+        for c in config['app']['considering_candles']:
+            exchange, symbol = c[0], c[1]
+            required_candles.inject_required_candles_to_store(
+                required_candles.load_required_candles(exchange, symbol, start_date_str, finish_date_str),
+                exchange,
+                symbol
+            )
+
+    # download candles for the duration of the backtest
+    candles = {}
+    for c in config['app']['considering_candles']:
+        exchange, symbol = c[0], c[1]
+
+        key = jh.key(exchange, symbol)
+
+        cache_key = f"{start_date_str}-{finish_date_str}-{key}"
+        cached_value = cache.get_value(cache_key)
+        # if cache exists use cache_value
+        # not cached, get and cache for later calls in the next 5 minutes
+        # fetch from database
+        candles_tuple = cached_value or Candle.select(
+                Candle.timestamp, Candle.open, Candle.close, Candle.high, Candle.low,
+                Candle.volume
+            ).where(
+                Candle.exchange == exchange,
+                Candle.symbol == symbol,
+                Candle.timeframe == '1m' or Candle.timeframe.is_null(),
+                Candle.timestamp.between(start_date, finish_date)
+            ).order_by(Candle.timestamp.asc()).tuples()
+        # validate that there are enough candles for selected period
+        required_candles_count = (finish_date - start_date) / 60_000
+        if len(candles_tuple) == 0 or candles_tuple[-1][0] != finish_date or candles_tuple[0][0] != start_date:
+            raise exceptions.CandleNotFoundInDatabase(
+                f'Not enough candles for {symbol}. You need to import candles.'
+            )
+        elif len(candles_tuple) != required_candles_count + 1:
+            raise exceptions.CandleNotFoundInDatabase(
+                f'There are missing candles between {start_date_str} => {finish_date_str}')
+
+        # cache it for near future calls
+        cache.set_value(cache_key, tuple(candles_tuple), expire_seconds=60 * 60 * 24 * 7)
+
+        candles[key] = {
+            'exchange': exchange,
+            'symbol': symbol,
+            'candles': np.array(candles_tuple)
+        }
+
+    return candles
+
+
+def simulator(
+        candles: dict,
+        run_silently: bool,
+        hyperparameters: dict = None,
+        generate_charts: bool = False,
+        generate_tradingview: bool = False,
+        generate_quantstats: bool = False,
+        generate_csv: bool = False,
+        generate_json: bool = False,
+        generate_equity_curve: bool = False,
+        generate_hyperparameters: bool = False,
+) -> dict:
+    result = {}
+    begin_time_track = time.time()
+    key = f"{config['app']['considering_candles'][0][0]}-{config['app']['considering_candles'][0][1]}"
+    first_candles_set = candles[key]['candles']
+    length = len(first_candles_set)
+    # to preset the array size for performance
+    try:
+        store.app.starting_time = first_candles_set[0][0]
+    except IndexError:
+        raise IndexError('Check your "warm_up_candles" config value')
+    store.app.time = first_candles_set[0][0]
+
+    # initiate strategies
+    for r in router.routes:
+        # if the r.strategy is str read it from file
+        if isinstance(r.strategy_name, str):
+            StrategyClass = jh.get_strategy_class(r.strategy_name)
+        # else it is a class object so just use it
+        else:
+            StrategyClass = r.strategy_name
+
+        try:
+            r.strategy = StrategyClass()
+        except TypeError:
+            raise exceptions.InvalidStrategy(
+                "Looks like the structure of your strategy directory is incorrect. Make sure to include the strategy INSIDE the __init__.py file. Another reason for this error might be that your strategy is missing the mandatory methods such as should_long(), go_long(), and should_cancel_entry(). "
+                "\nIf you need working examples, check out: https://github.com/jesse-ai/example-strategies"
+            )
+        except:
+            raise
+
+        r.strategy.name = r.strategy_name
+        r.strategy.exchange = r.exchange
+        r.strategy.symbol = r.symbol
+        r.strategy.timeframe = r.timeframe
+
+        # read the dna from strategy's dna() and use it for injecting inject hyperparameters
+        # first convert DNS string
