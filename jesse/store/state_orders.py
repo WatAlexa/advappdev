680607737ebfba585e39548e48f6bd4df2553ad9
WatAlexa <122657423+WatAlexa@users.noreply.github.com>
2022-11-20 10:50:14 +0000

    chore: updated README.md

--- jesse/store/state_orders.py
+++ jesse/store/state_orders.py
@@ -44,4 +44,82 @@ class OrdersState:
             o for o in self.storage[key] if o.id != order.id
         ]
 
-    def execute_pendi
+    def execute_pending_market_orders(self) -> None:
+        if not self.to_execute:
+            return
+
+        for o in self.to_execute:
+            o.execute()
+
+        self.to_execute = []
+
+    # # # # # # # # # # # # # # # # #
+    # getters
+    # # # # # # # # # # # # # # # # #
+    def get_orders(self, exchange, symbol) -> List[Order]:
+        key = f'{exchange}-{symbol}'
+        return self.storage.get(key, [])
+
+    def count_all_active_orders(self) -> int:
+        c = 0
+        for key in self.storage:
+            if len(self.storage[key]):
+                for o in self.storage[key]:
+                    if o.is_active:
+                        c += 1
+        return c
+
+    def count_active_orders(self, exchange: str, symbol: str) -> int:
+        orders = self.get_orders(exchange, symbol)
+
+        return sum(bool(o.is_active) for o in orders)
+
+    def count(self, exchange: str, symbol: str) -> int:
+        return len(self.get_orders(exchange, symbol))
+
+    def get_order_by_id(self, exchange: str, symbol: str, id: str, use_exchange_id: bool = False) -> Order:
+        key = f'{exchange}-{symbol}'
+
+        if use_exchange_id:
+            return pydash.find(self.storage[key], lambda o: o.exchange_id == id)
+
+        return pydash.find(self.storage[key], lambda o: o.id == id)
+
+    def get_entry_orders(self, exchange: str, symbol: str) -> List[Order]:
+        all_orders = self.get_orders(exchange, symbol)
+        # return empty if no orders
+        if len(all_orders) == 0:
+            return []
+        # return all orders if position is not opened yet
+        p = selectors.get_position(exchange, symbol)
+        if p.is_close:
+            entry_orders = all_orders.copy()
+        else:
+            entry_orders = [o for o in all_orders if o.side == jh.type_to_side(p.type)]
+
+        # exclude cancelled orders
+        entry_orders = [o for o in entry_orders if not o.is_canceled]
+
+        return entry_orders
+
+    def get_exit_orders(self, exchange: str, symbol: str) -> List[Order]:
+        """
+        excludes cancel orders but includes executed orders
+        """
+        all_orders = self.get_orders(exchange, symbol)
+        # return empty if no orders
+        if len(all_orders) == 0:
+            return []
+        # return empty if position is not opened yet
+        p = selectors.get_position(exchange, symbol)
+        if p.is_close:
+            return []
+        else:
+            exit_orders = [o for o in all_orders if o.side != jh.type_to_side(p.type)]
+
+        # exclude cancelled orders
+        exit_orders = [o for o in exit_orders if not o.is_canceled]
+
+        return exit_orders
+
+
