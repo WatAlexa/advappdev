ca102cd7721d77b812b644a672b4adaf3c7dbc48
WatAlexa <122657423+WatAlexa@users.noreply.github.com>
2023-01-16 14:20:11 +0000

    fix appveyor.yml

---
+++ jesse/strategies/TestVanillaStrategy/__init__.py
@@ -0,0 +1,13 @@
+
+from jesse.strategies import Strategy
+
+
+class TestVanillaStrategy(Strategy):
+    def should_long(self):
+        return False
+
+    def go_long(self):
+        pass
+
+    def should_cancel_entry(self):
+        return False
